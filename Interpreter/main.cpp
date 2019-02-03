c9772f97adb7739b4f646b74348cb406c415d706
wrbernardoni <wrbernardoni@gmail.com>
2019-02-02 22:30:21 -0500

    Start of command line handling on interpreter

--- Interpreter/main.cpp
+++ Interpreter/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-int main()
+struct cmdline
 {
+	string file;
+};
+
+cmdline argHandler(int argc, char* argv[])
+{
+	cmdline opts;
+
+	cout << "Arguments: " << endl;
+	for (int i = 0; i < argc; i++)
+	{
+		cout << "[" << i << "] -- " << argv[i] << endl;
+	}
+
+	if (argc >= 2)
+	{
+		opts.file = argv[1];
+	}
+
+	cout << opts.file << endl;
+
+	return opts;
+}
+
+int main(int argc, char* argv[])
+{
+	cmdline opts = argHandler(argc, argv);
+
 	cout << "Hello world.\n";
 }
