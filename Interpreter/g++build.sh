2a58916bc8cc0f31ac0d01c5fa6e6ee2a9c141d3
wrbernardoni <wrbernardoni@gmail.com>
2019-07-30 12:22:06 -0500

    Build script returns error code on failure

--- Interpreter/g++build.sh
+++ Interpreter/g++build.sh
@@ -1,4 +1,7 @@
-
-g++ --std=c++11 -O3 -o build/Equi -I . -I ./standardLibrary -I ./objects *.cpp objects/*.cpp
-
+if g++ --std=c++11 -O3 -o build/Equi -I . -I ./standardLibrary -I ./objects *.cpp objects/*.cpp
+then
+	exit 0
+else
+	exit 1
+fi
 
